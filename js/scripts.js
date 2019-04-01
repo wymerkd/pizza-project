0c0b05c6a256aee9d71cf8235ad48709aa8ea5b8
wymerkd <wymerkd@gmail.com>
2019-03-31 19:31:09 -0700

    replace checkbox with radio

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function Pizza(size, crust, toppings) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+}
+
+Pizza.protoype.Total = function() {
+  
+}
+
+// Pizza.prototype.pizzaTotal = function () {
+//   this.size + this.crust + this.toppings
+// }
+//
+// $(document).ready(function() {
+//
